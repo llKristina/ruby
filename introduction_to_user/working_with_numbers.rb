c8754f3dfed12481b60686f8736d6bbbea905f98
Kristina <kristina20052020@gmail.com>
2024-09-27 19:05:20 +0300

    lab 1 task 4.3
    Нахождение произведения таких делителей числа, сумма цифр
    которых меньше, чем сумма цифр исходного числа.

--- introduction_to_user/working_with_numbers.rb
+++ introduction_to_user/working_with_numbers.rb
@@ -33,10 +33,32 @@ def digits_greater_than_3(number)
   count
 end
 
+# Функция для нахождения суммы цифр числа
+def sum_of_digits(number)
+  number.to_s.chars.map(&:to_i).sum  # Преобразуем число в строку, разбиваем на символы и суммируем
+end
+
+# Функция для нахождения произведения делителей, сумма цифр которых меньше суммы цифр исходного числа
+def product_of_divisors(number)
+  digit_sum = sum_of_digits(number)  
+  product = 1 
+  has_valid_divisor = false  
+
+  (1..number).each do |i|  
+    if number % i == 0 && sum_of_digits(i) < digit_sum  # Проверяем, является ли i делителем и его сумма цифр меньше
+      product *= i  
+      has_valid_divisor = true  # Устанавливаем флаг, что найден хотя бы один корректный делитель
+    end
+  end
+
+  has_valid_divisor ? product : 0  # Если были найденные делители, возвращаем произведение, иначе 0
+end
+
 puts "Введите число:"
 number = gets.to_i 
 result = summa_of_prime_divisors(number)
 odd_digits_count = digits_greater_than_3(number)
+product = product_of_divisors(number)
 puts "Сумма простых делителей числа #{number} равна #{result}."
 puts "Количество нечетных цифр, больших 3, в числе #{number} равно #{odd_digits_count}."
-
+puts "Произведение делителей числа #{number}, сумма цифр которых меньше суммы цифр этого числа, равно #{product}."

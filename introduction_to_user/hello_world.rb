cb63f1527e5f91f9b524cc8d96859a1f7271a8f4
Kristina <kristina20052020@gmail.com>
2024-09-27 17:52:37 +0300

    lab 1 task 2
    Имя пользователя как аргумента программы с командной строки и опрос любимого языка программирования

--- introduction_to_user/hello_world.rb
+++ introduction_to_user/hello_world.rb
@@ -1,1 +1,18 @@
 puts "Hello,World!"
+name=ARGV[0]
+puts "Привет, #{name}"
+puts "Какой твой любимый язык программирования?"
+favLang=STDIN.gets.chomp.downcase
+if favLang == "ruby"
+  puts "Подлиза!"
+else
+  puts "Скоро будет Ruby :)"
+end
+case favLang.downcase
+when "python" 
+puts "Замечательный выбор!"
+when "c++" 
+puts "Круть))"
+when "c#" 
+puts "Здорово!!!"
+end

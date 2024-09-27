592f05e5ddf9396dc222ce62df6b0657839d6bb5
Kristina <kristina20052020@gmail.com>
2024-09-27 19:48:47 +0300

    lab 1 task 5.2
    Нахождение максимального элемента массива с использованием цикла for

--- introduction_to_user/min_max_first_positive.rb
+++ introduction_to_user/min_max_first_positive.rb
@@ -10,9 +10,25 @@ def find_minimum(array)
   min_value
 end
 
+# Метод для нахождения максимального элемента массива с использованием for
+def find_maximum(array)
+  return nil if array.empty?  # Проверяем, что массив не пустой
+  max_value = array[0]  # Инициализируем максимальное значение первым элементом
+
+  for i in 1...array.size  # Итерируемся по элементам массива
+    max_value = array[i] if array[i] > max_value  # Сравниваем и обновляем максимальное значение
+  end
+
+  max_value
+end
+
 array = [-3, 4, -2, 0, 1, -5, 7, -9]
 
 # Нахождение минимального элемента
 min_value = find_minimum(array)
 puts "Минимальный элемент: #{min_value}"
+
+# Нахождение максимального элемента
+max_value = find_maximum(array)
+puts "Максимальный элемент: #{max_value}"
 

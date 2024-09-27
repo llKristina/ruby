b8f2880f54aa893be780d822cb4b9ef926b32a05
Kristina <kristina20052020@gmail.com>
2024-09-27 18:40:56 +0300

    lab 1 task 4.2
    Нахождение количества нечетных цифр числа, больших 3

--- introduction_to_user/working_with_numbers.rb
+++ introduction_to_user/working_with_numbers.rb
@@ -18,7 +18,25 @@ def summa_of_prime_divisors(number)
   prime_divisors.sum
 end
 
+# Функция для подсчета нечетных цифр, больших 3
+def digits_greater_than_3(number)
+  count = 0
+  # Преобразуем число в строку и перебираем каждую цифру
+  number.to_s.each_char do |char|
+    digit = char.to_i  # Преобразуем символ обратно в целое число
+    # Проверяем условия: нечетное и больше 3
+    if digit.odd? && digit > 3
+      count += 1
+    end
+  end
+
+  count
+end
+
 puts "Введите число:"
 number = gets.to_i 
 result = summa_of_prime_divisors(number)
+odd_digits_count = digits_greater_than_3(number)
 puts "Сумма простых делителей числа #{number} равна #{result}."
+puts "Количество нечетных цифр, больших 3, в числе #{number} равно #{odd_digits_count}."
+

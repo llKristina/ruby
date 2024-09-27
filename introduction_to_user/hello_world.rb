fbb3fbc7a6a6bc716c7aece43ae7a8de48c84b17
Kristina <kristina20052020@gmail.com>
2024-09-27 18:04:56 +0300

    lab 1 task 3
    Выполнение команд руби и команд операционной системы

--- introduction_to_user/hello_world.rb
+++ introduction_to_user/hello_world.rb
@@ -16,3 +16,7 @@ puts "Круть))"
 when "c#" 
 puts "Здорово!!!"
 end
+puts "Введите команду языка Ruby:"
+eval(STDIN.gets.chomp)
+puts "Введите команду операционной системы:"
+system(STDIN.gets.chomp)

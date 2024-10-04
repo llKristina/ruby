3e634a7295d1c2f10253458211e5a137d8e96581
Kristina <kristina20052020@gmail.com>
2024-10-04 15:14:04 +0300

    lab 2 task 8
    Создание валидаций для остальных полей

--- class_student/Students.rb
+++ class_student/Students.rb
@@ -5,23 +5,72 @@ attr_accessor :id, :last_name, :first_name, :middle_name,
 def initialize(id:nil, last_name:, first_name:, middle_name:, phone:nil,
  telegram:nil, email:nil, github:nil)
 @id=id
-@last_name=last_name
-@first_name=first_name
-@middle_name=middle_name
+
+if self.class.valid_name?(last_name)
+	@last_name=last_name
+else
+	raise ArgumentError, "Недопустимая фамилия: #{last_name}"
+end
+	
+if self.class.valid_name?(first_name)
+	@first_name=first_name
+else
+	raise ArgumentError, "Недопустимое имя : #{first_name}"
+end
+
+if self.class.valid_name?(middle_name)
+	@middle_name=middle_name
+else
+	raise ArgumentError, "Недопустимое отчество : #{middle_name}"
+end
+
 if phone.nil? || self.class.valid_phone?(phone)
-      @phone = phone
-    else
-      raise ArgumentError, "Недопустимый номер телефона: #{phone}"
-    end
-@telegram=telegram
-@email=email
-@github=github
+    @phone = phone
+else
+    raise ArgumentError, "Недопустимый номер телефона: #{phone}"
+end
+
+if telegram.nil? || self.class.valid_telegram?(telegram)
+	@telegram=telegram
+else 
+	raise ArgumentError, "Недопустимый телеграм: #{telegram}"
+end
+
+if email.nil? || self.class.valid_email?(email)
+    @email = email
+else
+    raise ArgumentError, "Недопустимый email: #{email}"
+end
+	
+if github.nil? || self.class.valid_github?(github)
+   @github = github
+else
+    raise ArgumentError, "Недопустимый GitHub: #{github}"
+end
+ 
 end
 
  def self.valid_phone?(phone)
    return true if phone.nil?
     !!(phone =~ /\A\+?[0-9]{10,15}\z/)
  end
+ def self.valid_name?(name)
+    !!(name =~ /\A[А-Яа-яёЁA-Za-z\-]{2,50}\z/)
+  end
+
+  def self.valid_telegram?(handle)
+    !!(handle =~ /\A@[A-Za-z0-9_]{5,32}\z/)
+  end
+
+  def self.valid_email?(email)
+    !!(email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+  end
+
+  def self.valid_github?(handle)
+    !!(handle =~ /\A[A-Za-z0-9\-]{1,39}\z/)
+  end
+end
+
 
 
  def to_s

a498c711998e8a9be34f1947d4165be3bdfe4b64
Kristina <kristina20052020@gmail.com>
2024-10-04 16:09:59 +0300

    lab 2 task 2.4
    Написание метода getInfo который возвращает краткую
    информацию о студенте – Фамилия и Инициалы; гит,
    связь

--- class_student/Students.rb
+++ class_student/Students.rb
@@ -1,60 +1,60 @@
 class Student
-attr_accessor :id, :last_name, :middle_name, :first_name
-attr_reader :phone, :telegram, :email, :github #Контакты теперь нельзя устанавливать напрямую
+  attr_accessor :id, :last_name, :middle_name, :first_name
+  attr_reader :phone, :telegram, :email, :github # Контакты теперь нельзя устанавливать напрямую
 
-def initialize(id:nil, last_name:, first_name:, middle_name:, phone:nil,
- telegram:nil, email:nil, github:nil)
-@id=id
+  def initialize(id:nil, last_name:, first_name:, middle_name:, phone:nil,
+                 telegram:nil, email:nil, github:nil)
+    @id = id
 
-if self.class.valid_name?(last_name)
-	@last_name=last_name
-else
-	raise ArgumentError, "Недопустимая фамилия: #{last_name}"
-end
-	
-if self.class.valid_name?(first_name)
-	@first_name=first_name
-else
-	raise ArgumentError, "Недопустимое имя : #{first_name}"
-end
+    if self.class.valid_name?(last_name)
+      @last_name = last_name
+    else
+      raise ArgumentError, "Недопустимая фамилия: #{last_name}"
+    end
 
-if self.class.valid_name?(middle_name)
-	@middle_name=middle_name
-else
-	raise ArgumentError, "Недопустимое отчество : #{middle_name}"
-end
+    if self.class.valid_name?(first_name)
+      @first_name = first_name
+    else
+      raise ArgumentError, "Недопустимое имя: #{first_name}"
+    end
 
-if phone.nil? || self.class.valid_phone?(phone)
-    @phone = phone
-else
-    raise ArgumentError, "Недопустимый номер телефона: #{phone}"
-end
+    if self.class.valid_name?(middle_name)
+      @middle_name = middle_name
+    else
+      raise ArgumentError, "Недопустимое отчество: #{middle_name}"
+    end
 
-if telegram.nil? || self.class.valid_telegram?(telegram)
-	@telegram=telegram
-else 
-	raise ArgumentError, "Недопустимый телеграм: #{telegram}"
-end
+    if phone.nil? || self.class.valid_phone?(phone)
+      @phone = phone
+    else
+      raise ArgumentError, "Недопустимый номер телефона: #{phone}"
+    end
 
-if email.nil? || self.class.valid_email?(email)
-    @email = email
-else
-    raise ArgumentError, "Недопустимый email: #{email}"
-end
-	
-if github.nil? || self.class.valid_github?(github)
-   @github = github
-else
-    raise ArgumentError, "Недопустимый GitHub: #{github}"
-end
- 
-end
+    if telegram.nil? || self.class.valid_telegram?(telegram)
+      @telegram = telegram
+    else
+      raise ArgumentError, "Недопустимый телеграм: #{telegram}"
+    end
 
- def self.valid_phone?(phone)
-   return true if phone.nil?
+    if email.nil? || self.class.valid_email?(email)
+      @email = email
+    else
+      raise ArgumentError, "Недопустимый email: #{email}"
+    end
+
+    if github.nil? || self.class.valid_github?(github)
+      @github = github
+    else
+      raise ArgumentError, "Недопустимый GitHub: #{github}"
+    end
+  end
+
+  def self.valid_phone?(phone)
+    return true if phone.nil?
     !!(phone =~ /\A\+?[0-9]{10,15}\z/)
- end
- def self.valid_name?(name)
+  end
+
+  def self.valid_name?(name)
     !!(name =~ /\A[А-Яа-яёЁA-Za-z\-]{2,50}\z/)
   end
 
@@ -69,52 +69,73 @@ end
   def self.valid_github?(handle)
     !!(handle =~ /\A[A-Za-z0-9\-]{1,39}\z/)
   end
-  
-# Метод проверки наличия контакта для связи
- def contact_present?
+
+  # Метод проверки наличия контакта для связи
+  def contact_present?
     !(@phone.nil? && @telegram.nil? && @email.nil?)
- end
-
- def validate
-   unless contact_present?
-     raise ArgumentError, "Необходимо указать хотя бы один контакт для связи"
- end
-
-if @github.nil? || @github.empty?
-   raise ArgumentError, "Необходимо указать GitHub"
-end
-
-end
-
- # Метод установки контактов
- def set_contacts(phone: nil, telegram: nil, email: nil)
-   if phone
-     if self.class.valid_phone_number?(phone)
-      @phone = phone
-     else
-        raise ArgumentError, "Недопустимый номер телефона: #{phone}"
-     end
- end
-
- if telegram
-   if self.class.valid_telegram_handle?(telegram)
-      @telegram = telegram
-   else
-      raise ArgumentError, "Недопустимый телеграм: #{telegram}"
-   end
- end
-
- if email
-   if self.class.valid_email?(email)
-      @email = email
-   else
-      raise ArgumentError, "Недопустимый email: #{email}"
-   end
- end
-      validate
   end
 
-def to_s
+  def validate
+    unless contact_present?
+      raise ArgumentError, "Необходимо указать хотя бы один контакт для связи"
+    end
+
+    if @github.nil? || @github.empty?
+      raise ArgumentError, "Необходимо указать GitHub"
+    end
+  end
+
+  # Метод установки контактов
+  def set_contacts(phone: nil, telegram: nil, email: nil)
+    if phone
+      if self.class.valid_phone?(phone) # исправлено имя метода на valid_phone
+        @phone = phone
+      else
+        raise ArgumentError, "Недопустимый номер телефона: #{phone}"
+      end
+    end
+
+    if telegram
+      if self.class.valid_telegram?(telegram) # исправлено имя метода на valid_telegram
+        @telegram = telegram
+      else
+        raise ArgumentError, "Недопустимый телеграм: #{telegram}"
+      end
+    end
+
+    if email
+      if self.class.valid_email?(email)
+        @email = email
+      else
+        raise ArgumentError, "Недопустимый email: #{email}"
+      end
+    end
+
+    validate
+  end
+
+  # Метод получения информации
+  def getInfo
+    "#{last_name_with_initials}; GitHub: #{@github}; Contact: #{preferred_contact}"
+  end
+
+  def last_name_with_initials
+    "#{@last_name} #{initials}"
+  end
+
+  def preferred_contact
+    if @phone
+      "Phone: #{@phone}"
+    elsif @telegram
+      "Telegram: #{@telegram}"
+    elsif @email
+      "Email: #{@email}"
+    else
+      "No contact available"
+    end
+  end
+  
+ def to_s
     str = []
     str.push("ID: #{@id}") if @id
     str.push("Фамилия: #{@last_name}")
@@ -125,6 +146,11 @@ def to_s
     str.push("Почта: #{@email}") if @email
     str.push("GitHub: #{@github}") if @github
     str.join("\n")
-  end
+ end
+  
+  private
 
+  def initials
+    "#{@first_name[0]}. #{@middle_name[0]}."
+  end
 end

47a98ddb82e0ed908263b0e69ef515a91ab3f52b
Kristina <kristina20052020@gmail.com>
2024-10-08 17:09:00 +0300

    lab 2 task 3,4,5
    Использование атрибутов для краткого описания геттеров и сеттеров

--- class_student/student.rb
+++ class_student/student.rb
@@ -1,76 +1,27 @@
 class Student
-def initialize(id:nil, last_name:, first_name:, middle_name:, phone:nil,
- telegram:nil, email:nil, github:nil)
-@id=id
-@last_name=last_name
-@first_name=first_name
-@middle_name=middle_name
-@phone=phone
-@telegram=telegram
-@email=email
-@github=github
-end
-#геттеры
-def get_id
- @id
-end
-def get_last_name
- @last_name
-end
-def get_first_name
- @first_name
-end
-def get_middle_name
- @middle_name
-end
-def get_phone
- @phone
-end
-def get_telegram
- @telegram
-end
-def get_email
- @email
-end
-def get_github
- @github
-end
-#сеттеры
-def set_id(id)
- @id=id
-end
-def set_last_name(last_name)
- @last_name=last_name
-end
-def set_first_name(first_name)
-  @first_name = first_name
- end
-def set_middle_name(middle_name)
- @middle_name = middle_name
-end
-def set_phone(phone)
- @phone = phone
-end
-def set_telegram(telegram)
- @telegram = telegram
-end
-def set_email(email)
- @email = email
-end
-def set_github(github)
- @github = github
-end
-def to_s
+  attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
+
+  def initialize(id:nil, last_name:, first_name:, middle_name:, phone:nil, telegram:nil, email:nil, github:nil)
+    @id = id
+    @last_name = last_name
+    @first_name = first_name
+    @middle_name = middle_name
+    @phone = phone
+    @telegram = telegram
+    @email = email
+    @github = github
+  end
+
+  def to_s
     str = []
-    str.push ("ID: #{@id}" ) 
+    str.push("ID: #{@id}") if @id
     str.push("Фамилия: #{@last_name}")
     str.push("Имя: #{@first_name}")
     str.push("Отчество: #{@middle_name}")
-    str.push("Телефон: #{@phone}") 
-    str.push("Телеграм: #{@telegram}" ) 
-    str.push("Почта: #{@email}" )
-    str.push("GitHub: #{@github}") 
-    return str.join("\n")
- end
- 
+    str.push("Телефон: #{@phone}") if @phone
+    str.push("Телеграм: #{@telegram}") if @telegram
+    str.push("Почта: #{@email}") if @email
+    str.push("GitHub: #{@github}") if @github
+    str.join("\n")
+  end
 end

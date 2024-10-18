cdff3622b67494c2d672991f8e20a458ff82a8ad
Kristina <kristina20052020@gmail.com>
2024-10-18 13:07:07 +0300

    lab 2
    исправление недочетов

--- class_student/main.rb
+++ class_student/main.rb
@@ -2,11 +2,11 @@ require_relative 'student'
 
 begin
   student1 = Student.new(id: 1, last_name: "Капустин", first_name: "Андрей", 
-  middle_name: "Владимирович", phone: "89183456789", telegram: "@kapusta",
+  middle_name: "Владимирович", phone: '+79894032567', telegram: "@kapusta", 
   email: "andreyV@example.com", github: "kapustaVV")
 
-  student2 = Student.new(last_name: "Петрова", first_name: "Мария", middle_name: "Андреевна", 
-  github: "mariaGitHub")
+  student2 = Student.new(last_name: "Петрова", first_name: "Мария",  
+  middle_name: "Андреевна",phone: '+79824162767', github: "mariaGitHub")
 
   puts "Информация о студенте 1:"
   puts student1.to_s

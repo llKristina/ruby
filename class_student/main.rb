00dc6b028771e1f8f0b88dc83aadacdcea4c8a21
Kristina <kristina20052020@gmail.com>
2024-10-18 14:35:48 +0300

    lab 2 task 4
    Создание класса Student_short

--- class_student/main.rb
+++ class_student/main.rb
@@ -7,12 +7,18 @@ begin
 
   student2 = Student.new(last_name: "Петрова", first_name: "Мария",  
   middle_name: "Андреевна",phone: '+79824162767', github: "mariaGitHub")
-
+  
   puts "Информация о студенте 1:"
   puts student1.to_s
 
   puts "\n**Информация о студенте 2:"
   puts student2.to_s
+  
+  student_short2 = Student_short.new(student2.id, "#{student2.last_name_initials};
+  GitHub: #{student2.github}; Контакт: #{student2.contact_info}")
+
+  puts "\nИнформация о студенте 2 (короткая):"
+  puts student_short2.to_s
 
 rescue ArgumentError => e
   puts "Ошибка создания студента:"
--- class_student/student.rb
+++ class_student/student.rb
@@ -89,3 +89,36 @@ class Student
     !(@phone.nil? && @telegram.nil? && @email.nil?)
   end
 end
+
+class Student_short
+ attr_reader :id, :last_name_initials, :github, :contact
+
+ # Конструктор для объекта Student
+ def initialize(student)
+  @id = student.id
+  @last_name_initials = student.last_name_initials
+  @github = student.github
+  @contact = student.contact_info
+ end
+
+ # Конструктор для id и строки info
+ def initialize(id, info)
+  @id = id
+  @last_name_initials, @github, @contact = parse_info(info)
+ end
+
+def to_s
+  "ID: #{@id}, Фамилия и инициалы: #{@last_name_initials}, GitHub: #{@github}, Контакт: #{@contact}"
+ end
+ 
+ private
+
+ def parse_info(info)
+  parts = info.split(';')
+  last_name_initials = parts[0].strip
+  github = parts[1].split(':')[1].strip
+  contact = parts[2].split(':')[1].strip
+  [last_name_initials, github, contact]
+ end
+end
+

34b69f2efd63aca9690f160db5806db61993a5ac
Kristina <kristina20052020@gmail.com>
2024-10-25 15:42:45 +0300

    lab 3 task 1.4
    вывод положительных простых делителей

--- working_with_arrays/massiv_task1.rb
+++ working_with_arrays/massiv_task1.rb
@@ -14,7 +14,7 @@ def find_max(array,a,b)
     return
   end
 	max=array[a..b].max
-	puts " Максимальный элемент в интервале: #{max}"
+	puts "Максимальный элемент в интервале: #{max}"
 end
 
 #Дан целочисленный массив. Вывести индексы элементов, которые
@@ -31,8 +31,33 @@ def less_than_neighbor(array)
 puts "Индексы элементов меньше левого соседа: #{index}"
 puts "Количество таких элементов: #{count}"
 end
+
+#Для введенного списка положительных чисел построить список всех
+#положительных простых делителей элементов списка без повторений.
+def find_prime_divisors(numbers)
+	prime_divisors=[]
+	def is_prime?(number)
+		return false if number<=1
+		(2..Math.sqrt(number)).each do |i|
+			return false if number%i==0
+		end
+		true
+	end
+	
+	numbers.each do |number|
+    (2..number).each do |divisor|
+      if number % divisor == 0 && is_prime?(divisor) && !prime_divisors.include?(divisor)
+        prime_divisors << divisor
+      end
+    end
+  end
+  puts "Список всех простых делителей: #{prime_divisors}"
+end
+	
+
 arr=[5,7,8,3,1,8,6,12]
 
 rearrange_array(arr)
 find_max(arr,1,4)
 less_than_neighbor(arr)
+find_prime_divisors(arr)

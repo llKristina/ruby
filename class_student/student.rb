4d797fc96f7f5383a71423c6a9e30de8f8b5f982
Kristina <kristina20052020@gmail.com>
2024-11-07 14:34:31 +0300

    lab 2 task 1
    Создание класса Student, создание конструктора, геттеров и сеттеров

--- class_student/student.rb
+++ class_student/student.rb
@@ -1,23 +1,85 @@
-require_relative 'person'
-class Student < Person
- def get_info
-    "#{last_name} #{first_name[0]}#{middle_name[0]}; GitHub: #{github}; Контакт: #{contact_info}"
+class Student
+  # Инициализация объекта Student с обязательными и необязательными полями
+  def initialize(surname, name, patronymic, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    @surname = surname
+    @name = name
+    @patronymic = patronymic
+    @id = id
+    @phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
   end
 
-  def last_name_initials
-    "#{last_name} #{first_name[0]}#{middle_name[0]}"
+  # Геттеры и сеттеры для ID
+  def id
+    @id
   end
 
-  def to_s
-    str = []
-    str.push("ID: #{@id}") if @id
-    str.push("Фамилия: #{@last_name}")
-    str.push("Имя: #{@first_name}")
-    str.push("Отчество: #{@middle_name}")
-    str.push("Телефон: #{@phone}") if @phone
-    str.push("Телеграм: #{@telegram}") if @telegram
-    str.push("Почта: #{@email}") if @email
-    str.push("GitHub: #{@github}") if @github
-    str.join("\n")
+  def id=(value)
+    @id = value
+  end
+
+  # Геттеры и сеттеры для Фамилии
+  def surname
+    @surname
+  end
+
+  def surname=(value)
+    @surname = value
+  end
+
+  # Геттеры и сеттеры для Имени
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
+  end
+
+  # Геттеры и сеттеры для Отчества
+  def patronymic
+    @patronymic
+  end
+
+  def patronymic=(value)
+    @patronymic = value
+  end
+
+  # Геттеры и сеттеры для Телефона
+  def phone
+    @phone
+  end
+
+  def phone=(value)
+    @phone = value
+  end
+
+  # Геттеры и сеттеры для Телеграм
+  def telegram
+    @telegram
+  end
+
+  def telegram=(value)
+    @telegram = value
+  end
+
+  # Геттеры и сеттеры для Почты
+  def email
+    @email
+  end
+
+  def email=(value)
+    @email = value
+  end
+
+  # Геттеры и сеттеры для Гит
+  def git
+    @git
+  end
+
+  def git=(value)
+    @git = value
   end
 end

94cbebcc765d72144ca96660c413f24a91366b6d
Kristina <kristina20052020@gmail.com>
2024-11-07 14:51:35 +0300

    lab 2 task 3,4
    Использование атрибутов для краткого написания геттеров и сеттеров

--- class_student/student.rb
+++ class_student/student.rb
@@ -1,85 +1,19 @@
 class Student
-  # Инициализация объекта Student с обязательными и необязательными полями
+  # Объявляем геттеры и сеттеры 
+  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+
+  # Конструктор
   def initialize(surname, name, patronymic, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    # Устанавливаем обязательные параметры
     @surname = surname
     @name = name
     @patronymic = patronymic
-    @id = id
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
-  end
 
-  # Геттеры и сеттеры для ID
-  def id
-    @id
-  end
-
-  def id=(value)
-    @id = value
-  end
-
-  # Геттеры и сеттеры для Фамилии
-  def surname
-    @surname
-  end
-
-  def surname=(value)
-    @surname = value
-  end
-
-  # Геттеры и сеттеры для Имени
-  def name
-    @name
-  end
-
-  def name=(value)
-    @name = value
-  end
-
-  # Геттеры и сеттеры для Отчества
-  def patronymic
-    @patronymic
-  end
-
-  def patronymic=(value)
-    @patronymic = value
-  end
-
-  # Геттеры и сеттеры для Телефона
-  def phone
-    @phone
-  end
-
-  def phone=(value)
-    @phone = value
-  end
-
-  # Геттеры и сеттеры для Телеграм
-  def telegram
-    @telegram
-  end
-
-  def telegram=(value)
-    @telegram = value
-  end
-
-  # Геттеры и сеттеры для Почты
-  def email
-    @email
-  end
-
-  def email=(value)
-    @email = value
-  end
-
-  # Геттеры и сеттеры для Гит
-  def git
-    @git
-  end
-
-  def git=(value)
-    @git = value
+    # Устанавливаем необязательные параметры, проверяя на наличие значений
+    @id = id unless id.nil?
+    @phone = phone unless phone.nil?
+    @telegram = telegram unless telegram.nil?
+    @email = email unless email.nil?
+    @git = git unless git.nil?
   end
 end

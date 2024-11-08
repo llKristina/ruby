5172b04b30b1305f5930cbe9dc5512097388966f
Kristina <kristina20052020@gmail.com>
2024-11-08 14:15:28 +0300

    lab 2 task 6
    Использование ХЭШа

--- class_student/main.rb
+++ class_student/main.rb
@@ -1,21 +1,8 @@
 require_relative 'student'
 
-# Создание объекта Student с обязательными полями
-student = Student.new('Иванов', 'Иван', 'Иванович')
+student1 = Student.new(surname: "Иванов", name: "Иван", patronymic: "Иванович")
 
-# Установка дополнительных полей
-student.id = 1
-student.phone = '+7-999-123-45-67'
-student.telegram = '@ivanov'
-student.email = 'ivanov@example.com'
-student.git = 'github.com/ivanov'
-
-# Вывод информации о студенте
-puts "ID: #{student.id}"
-puts "Фамилия: #{student.surname}"
-puts "Имя: #{student.name}"
-puts "Отчество: #{student.patronymic}"
-puts "Телефон: #{student.phone}"
-puts "Телеграм: #{student.telegram}"
-puts "Почта: #{student.email}"
-puts "Git: #{student.git}"
+student2 = Student.new(surname: "Иванов", name: "Иван", patronymic: "Иванович", id: 1, phone: "123-456-789", 
+telegram: "@ivanov", email: "ivanov@example.com", git: "github.com/ivanov")
+puts student1
+puts student2
--- class_student/student.rb
+++ class_student/student.rb
@@ -2,18 +2,27 @@ class Student
   # Объявляем геттеры и сеттеры 
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
-  # Конструктор
-  def initialize(surname, name, patronymic, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-    # Устанавливаем обязательные параметры
-    @surname = surname
-    @name = name
-    @patronymic = patronymic
+  # Конструктор принимает аргументы в виде хэша
+  def initialize(attributes = {})
+    # Устанавливаем обязательные поля
+    @surname = attributes[:surname] 
+    @name = attributes[:name] 
+    @patronymic = attributes[:patronymic] 
 
-    # Устанавливаем необязательные параметры 
-    @id = id
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+    # Устанавливаем необязательные поля
+    @id = attributes[:id]
+    @phone = attributes[:phone]
+    @telegram = attributes[:telegram]
+    @email = attributes[:email]
+    @git = attributes[:git]
+  end
+  
+   def to_s
+   "Student: #{@surname} #{@name} #{@patronymic}
+    ID: #{@id} 
+	Phone: #{@phone}
+    Telegram: #{@telegram} 
+	Email: #{@email}
+	Git: #{@git}"
   end
 end

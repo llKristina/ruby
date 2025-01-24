31833ead9d2309ae9ad83dbf2657e791280c83e7
llKristina <kristina20052020@gmail.com>
2025-01-24 15:32:13 +0300

    lab 4 update diagram

--- class_student/database/Student_list_DB.rb
+++ class_student/database/Student_list_DB.rb
@@ -1,6 +1,6 @@
 require_relative 'C:\Users\admin\Documents\GitHub\ruby\class_student\studentShort.rb'
 require_relative 'C:\Users\admin\Documents\GitHub\ruby\class_student\data\Data_list_student_short.rb'
-require_relative 'student_database'
+
 
 class StudentsListDB
   def initialize
